9cb99005c4ce20fd76ba5785958524d429c6b7d3
qing-del <1244966847@qq.com>
2026-04-02 23:10:13 +0800

    Init
    初始化项目

---
+++ middleware-server/src/main/java/com/jacolp/MiddlewareServerApplication.java
@@ -0,0 +1,13 @@
+package com.jacolp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiddlewareServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiddlewareServerApplication.class, args);
+    }
+
+}
