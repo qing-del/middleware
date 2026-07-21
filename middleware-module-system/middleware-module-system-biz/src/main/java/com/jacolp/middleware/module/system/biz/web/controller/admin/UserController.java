151ca15c3ee532a5c7f2f63e0d0d22393e95590c
qing-del <1244966847@qq.com>
2026-07-21 10:42:15 +0800

    chore: remove obsolete redis import

--- middleware-module-system/middleware-module-system-biz/src/main/java/com/jacolp/middleware/module/system/biz/web/controller/admin/UserController.java
+++ middleware-module-system/middleware-module-system-biz/src/main/java/com/jacolp/middleware/module/system/biz/web/controller/admin/UserController.java
@@ -19,7 +19,6 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
